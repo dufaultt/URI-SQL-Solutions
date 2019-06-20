94985c5e70723b8346c7e3c0a6f75b66082f1636
Tristan Dufault <32806910+dufaultt@users.noreply.github.com>
2019-06-19 23:26:17 -0400

    Update URI-2742.sql
    Adding comments

--- URI-2742.sql
+++ URI-2742.sql
@@ -1,3 +1,10 @@
+/*
+As your first task, you must select every Richard from dimensions C875 and C774, 
+together with its existence probability (the famous factor N) with three decimal places of precision.
+
+Remember that the N factor is calculated by multiplying the omega value by 1,618. 
+The data must be sorted by the least omega value.
+*/
 select life_registry.name, round((life_registry.omega*1.618),3) as "The N Factor"
 from life_registry
 join dimensions on dimensions.id = life_registry.dimensions_id
